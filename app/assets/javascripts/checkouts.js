d6cddb93c92110aeea17051648d526520d2e2574
Joe Greathead <joegreathead@yahoo.com>
2013-11-24 18:01:27 -0500

    Change checkout js ids to be different than other pages.

--- app/assets/javascripts/checkouts.js
+++ app/assets/javascripts/checkouts.js
@@ -1,19 +1,19 @@
 var enableCheckoutButton = function() {
-  $("#checkout_btn").prop("disabled", !v_g || !v_a);
-  $("#swap_btn").prop("disabled", (!v_g || !v_a) || (c_o != 1));
-  $('#x_atte').toggleClass('invis', !v_a);
-  $('#x_game').toggleClass('invis', !v_g);
+	$("#checkout_btn").prop("disabled", !v_g || !v_a);
+	$("#swap_btn").prop("disabled", (!v_g || !v_a) || (c_o != 1));
+	$('#x_atte').toggleClass('invis', !v_a);
+	$('#x_game').toggleClass('invis', !v_g);
 };
 
 var resetCheckout = function() {
-  $("#a_id").attr('readonly', false);
-  $("#a_label").text('');
-  $("#g_label").text('');
-  $("#a_id").val('');
-  $("#g_id").val('');
-  v_a = false;
-  v_g = false;
-  c_o = 0;
+	$("#co_a_id").attr('readonly', false);
+	$("#a_label").text('');
+	$("#g_label").text('');
+	$("#co_a_id").val('');
+	$("#co_g_id").val('');
+	v_a = false;
+	v_g = false;
+	c_o = 0;
 };
 
 var bc_regex = /^[a-z]{3}[a-z0-9]{3,6}$/i;
@@ -23,128 +23,139 @@ var c_o = 0;
 
 $(document).ready(function() {
 
-  $("#a_id").change(function(e) {
-    if (bc_regex.test(this.value)) {
-      $.ajax({
-        url : "/is_valid_attendee",
-        data : {
-          a_id : this.value
-        },
-        dataType : "json",
-        complete : enableCheckoutButton,
-        success : function(data) {
-          if ( v_a = data.valid) {
-            $("#g_id").toggleClass('invis', false);
-            $("#g_id").focus();
-          } else {
-            $('#new_attendee').dialog('open');
-          }
-          $("#a_id").attr('readonly', data.valid);
-          $("#a_label").text(data.message);
-          c_o = data.coCount;
-        }
-      });
-    }
-  });
+	//entry for attendee barcode
+	$("#co_a_id").change(function(e) {
+		if (bc_regex.test(this.value)) {
+			$.ajax({
+				url : "/is_valid_attendee",
+				data : {
+					a_id : this.value
+				},
+				dataType : "json",
+				complete : enableCheckoutButton,
+				success : function(data) {
+					if ( v_a = data.valid) {
+						$("#co_g_id").toggleClass('invis', false);
+						$("#co_g_id").focus();
+					} else {
+						$('#new_attendee').dialog('open');
+					}
+					$("#co_a_id").attr('readonly', data.valid);
+					$("#a_label").text(data.message);
+					c_o = data.coCount;
+				}
+			});
+		}
+	});
 
-  $("#g_id").change(function(e) {
-    if (bc_regex.test(this.value)) {
-      $.ajax({
-        url : "/is_valid_game",
-        data : {
-          g_id : this.value
-        },
-        dataType : "json",
-        complete : enableCheckoutButton,
-        success : function(data) {
-          $("#g_label").text(data.message);
-          v_g = data.valid;
-        }
-      });
-    }
-  });
+	//entry for game barcode
+	$("#co_g_id").change(function(e) {
+		if (bc_regex.test(this.value)) {
+			$.ajax({
+				url : "/is_valid_game",
+				data : {
+					g_id : this.value
+				},
+				dataType : "json",
+				complete : enableCheckoutButton,
+				success : function(data) {
+					$("#g_label").text(data.message);
+					v_g = data.valid;
+				}
+			});
+		}
+	});
 
-  $('#checkout_btn').click(function() {
-    //data = $('input[type="text"]').serializeArray();
-    data = $('input').serializeArray();
-    $.ajax({
-      url : '/checkouts/create',
-      data : data,
-      dataType : 'json',
-      type : 'POST',
-      complete : enableCheckoutButton,
-      success : function(data) {
-        if (data.success) {
-          resetCheckout();
-          //TODO: display success message
-        }
-      }
-    });
-  });
+	//checkout button for new checkouts
+	$('#checkout_btn').click(function() {
+		data = {
+			g_id : $('#co_g_id').val(),
+			a_id : $('#co_a_id').val()
+		};
+		$.ajax({
+			url : '/checkouts/create',
+			data : data,
+			dataType : 'json',
+			type : 'POST',
+			complete : enableCheckoutButton,
+			success : function(data) {
+				if (data.success) {
+					resetCheckout();
+					//TODO: display success message
+				}
+			}
+		});
+	});
 
-  $('#swap_btn').click(function() {
-    data = $('input[type="text"]').serializeArray();
-    data.swap = true;
-    $.ajax({
-      url : '/checkouts/swap',
-      data : data,
-      dataType : 'json',
-      type : 'POST',
-      complete : enableCheckoutButton,
-      success : function(data) {
-        if (data.success) {
-          resetCheckout();
-          //TODO: display success message
-        }
-      }
-    });
-  });
+	//swap button for new checkouts
+	$('#swap_btn').click(function() {
+		data = {
+			g_id : $('#co_g_id').val(),
+			a_id : $('#co_a_id').val()
+		};
+		data.swap = true;
+		$.ajax({
+			url : '/checkouts/swap',
+			data : data,
+			dataType : 'json',
+			type : 'POST',
+			complete : enableCheckoutButton,
+			success : function(data) {
+				if (data.success) {
+					resetCheckout();
+					//TODO: display success message
+				}
+			}
+		});
+	});
 
-  $('#x_atte').click(function() {
-    $("#a_id").attr('readonly', false);
-    $("#a_id").val('');
-    $("#a_label").text('');
-    $('#new_attendee').toggleClass('invis', true);
-    $('#new_attendee > input').val('');
-    v_a = false;
-  });
-  $('#x_atte').click(enableCheckoutButton);
+	//X button for attendee barcode
+	$('#x_atte').click(function() {
+		$("#co_a_id").attr('readonly', false);
+		$("#co_a_id").val('');
+		$("#a_label").text('');
+		$('#new_attendee').toggleClass('invis', true);
+		$('#new_attendee > input').val('');
+		v_a = false;
+	});
+	$('#x_atte').click(enableCheckoutButton);
 
-  $('#x_game').click(function() {
-    //$("#g_id").attr('readonly', false); not needed currently
-    $("#g_id").val('');
-    $("#g_label").text('');
-    v_g = false;
-  });
-  $('#x_game').click(enableCheckoutButton);
-  
-  //modal dialog
-  $("#new_attendee").dialog({
-    autoOpen : false,
-    modal : true,
-    resizable: true,
-    buttons : {
-      "Add Attendee" : function() {
-        data = $('#new_attendee > input[type="text"], #a_id').serializeArray();
-        $.ajax({
-          url : '/attendees/create',
-          data : data,
-          dataType : 'json',
-          type : 'POST',
-          success : function(data) {
-            if (data.success) {
-              $("#new_attendee").dialog("close");
-              $('#a_id').change();
-            }
-          }
-        });
-      },
-      Cancel : function() {
-        $("#new_attendee").dialog("close");
-      }
-    },
-    close : function() {
-      $('#new_attendee > input[type="text"]').val('');
-    }
-  });
+	//X button for game barcode
+	$('#x_game').click(function() {
+		//$("#co_g_id").attr('readonly', false); not needed currently
+		$("#co_g_id").val('');
+		$("#g_label").text('');
+		v_g = false;
+	});
+	$('#x_game').click(enableCheckoutButton);
+
+	//modal dialog for new attendee
+	$("#new_attendee").dialog({
+		autoOpen : false,
+		modal : true,
+		resizable : true,
+		buttons : {
+			"Add Attendee" : function() {
+				data = $('#new_attendee > input[type="text"], #co_a_id').serializeArray();
+				$.ajax({
+					url : '/attendees/create',
+					data : data,
+					dataType : 'json',
+					type : 'POST',
+					success : function(data) {
+						if (data.success) {
+							$("#new_attendee").dialog("close");
+							$('#co_a_id').change();
+						}
+					}
+				});
+			},
+			Cancel : function() {
+				$("#new_attendee").dialog("close");
+			}
+		},
+		close : function() {
+			$('#new_attendee > input[type="text"]').val('');
+		}
+	});
 });
